e8bbcf1b52c9b5109d1dd2eab6fdb83a1fe5dc89
Diego Araujo <diegolaraujo96@gmail.com>
2023-03-30 18:47:20 -0300

    fix typo

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,1 @@
-export const url = 'development' ? 'http://localhost:3001' : 'http://test.sistemaslenox.com:49002';
+export const url = import.meta.env.MODE === 'development' ? 'http://localhost:3001' : 'http://test.sistemaslenox.com:49002';
