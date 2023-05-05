1ebc4116600b96fe4019a05287ceb6bd0b82aa7f
Diego Araujo <diegolaraujo96@gmail.com>
2023-05-05 14:20:16 -0300

    Changed buggy library of Icon component

--- src/Components/AppRow.tsx
+++ src/Components/AppRow.tsx
@@ -2,8 +2,6 @@ import roundUpdate from "@iconify/icons-ic/round-update"
 import gitBranch from "@iconify/icons-tabler/git-branch"
 import {Icon} from "@iconify/react"
 import { useStore } from "../Context/store"
-import IconMdi from '@mdi/react';
-import { mdiTextBoxOutline } from '@mdi/js';
 import "./modal.css"
 
  const AppRow = ({ app }: { app: string }) => {
@@ -14,32 +12,34 @@ import "./modal.css"
         setApp(app)
         e.target.classList.remove('loading')
     }
-    return <div className="app_row">
-        <div className="title" onClick={fetchTags}>
-            <p>
-                App: {app}
-            </p>
-        </div>
-        <div>
-            <div className="action" onClick={() => setCommitSelectModal(app)}>
-                <Icon icon={gitBranch} height="25" width="25"/>
-                Update
+    return (
+        <div className="app_row">
+            <div className="title" onClick={fetchTags}>
+                <p>
+                    App: {app}
+                </p>
             </div>
-            <div className="action" onClick={() => setAutoUpdateModal(app)}>
-                <Icon icon={roundUpdate} height="25" width="25"/>
-                <span>Auto-Update</span>
-                {autoUpdateModal.data[app] ? 
-                <div className={autoUpdateModal.data[app].Status === 2 ? "status_error" : "status"} >
-                    <span><span className="monospace">{autoUpdateModal.data[app]?.Branch}</span> cada </span>
-                    <span><span className="monospace">{autoUpdateModal.data[app]?.Seconds}</span> segundos</span>
-                    <div className="log_icon" onClick={(e) => {
-                            e.stopPropagation()
-                            setLogModal(app)
-                        }}><IconMdi path={mdiTextBoxOutline} size={1} /></div>
-                </div> : null}
+            <div>
+                <div className="action" onClick={() => setCommitSelectModal(app)}>
+                    <Icon icon={gitBranch} height="25" width="25"/>
+                    Update
+                </div>
+                <div className="action" onClick={() => setAutoUpdateModal(app)}>
+                    <Icon icon={roundUpdate} height="25" width="25"/>
+                    <span>Auto-Update</span>
+                    {autoUpdateModal.data[app] ? 
+                        <div className={autoUpdateModal.data[app].Status === 2 ? "status_error" : "status"} >
+                            <span><span className="monospace">{autoUpdateModal.data[app]?.Branch}</span> cada </span>
+                            <span><span className="monospace">{autoUpdateModal.data[app]?.Seconds}</span> segundos</span>
+                            <div className="log_icon" onClick={(e) => {
+                                e.stopPropagation()
+                                setLogModal(app)
+                            }}><Icon icon="ic:outline-text-snippet" /></div>
+                        </div> : null}
+                </div>
             </div>
         </div>
-    </div>
+    )
 } 
 
 export default AppRow
