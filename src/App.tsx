574f17062dc1156a440f0c704c734fb60e2bd9e7
Diego Araujo <diegolaraujo96@gmail.com>
2023-06-03 13:11:16 -0300

    Also fixing css.

--- src/App.tsx
+++ src/App.tsx
@@ -31,27 +31,29 @@ function App() {
     )
     if (isAuthenticated) return (
         <div className='repos'>
-            <button className='update' onClick={() => {
-                setUpdatingRemote(true)
-                axios.get(`${url}/updateRepos`)
-                    .then(() => {
-                        setApp(repo.name)
-                        setUpdatingRemote(false)
-                    })
-                    .catch((err) => {
-                        let message = ""
-                        if (err.response.data) {
-                            message = err.response.data.map(({Err}: {Err: UpdateReposError}) => `${Err.Err}\t${Err.Name}`)
-                        }
-                        alert("Ha ocurrido un error: " + message)
-                        setUpdatingRemote(false)
-                    })
-            }}>
-                {updatingRemote ? "Actualizando..." : "Actualizar repositorios"}
-            </button>
-            <div className="version_table">
-                <VersionPane />
-                <div className='app_rows'>{repos.map((repo, i) => <AppRow app={repo} key={i}/>)}</div>
+            <div className="update_pane">
+                <button className='update' onClick={() => {
+                    setUpdatingRemote(true)
+                    axios.get(`${url}/updateRepos`)
+                        .then(() => {
+                            setApp(repo.name)
+                            setUpdatingRemote(false)
+                        })
+                        .catch((err) => {
+                            let message = ""
+                            if (err.response.data) {
+                                message = err.response.data.map(({Err}: {Err: UpdateReposError}) => `${Err.Err}\t${Err.Name}`)
+                            }
+                            alert("Ha ocurrido un error: " + message)
+                            setUpdatingRemote(false)
+                        })
+                }}>
+                    {updatingRemote ? "Actualizando..." : "Actualizar repositorios"}
+                </button>
+                <div className="version_table">
+                    <VersionPane />
+                    <div className='app_rows'>{repos.map((repo, i) => <AppRow app={repo} key={i}/>)}</div>
+                </div>
             </div>
             <History/>
             {modal && <VersionChangeModal/>}
